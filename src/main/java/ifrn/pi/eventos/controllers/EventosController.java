bbb0573d261112c2fa8a7e615a4619cfb7f961bc
David <teixeira.david@escolar.ifrn.edu.br>
2026-08-20 13:00:58 -0300

    Cadastrar evento

--- src/main/java/ifrn/pi/eventos/controllers/EventosController.java
+++ src/main/java/ifrn/pi/eventos/controllers/EventosController.java
@@ -1,7 +1,10 @@
 package ifrn.pi.eventos.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import ifrn.pi.eventos.models.Evento;
 
 @Controller
 public class EventosController {
@@ -9,6 +12,18 @@ public class EventosController {
 	@RequestMapping("/eventos/form")
 	public String form() {
 		return "formEvento";
+	}
+	
+	@PostMapping("/evento")
+	public String cadastrarEvento(Evento evento) {
+		System.out.println("Método cadastrarEvento foi chamado!");
+		
+		System.out.println("Nome:" + evento.getNome());
+		System.out.println("Local:" + evento.getLocal());
+		System.out.println("Data:" + evento.getData());
+		System.out.println("Horário:" + evento.getHorario());
+		
+		return "home";
 	}
 
 }
